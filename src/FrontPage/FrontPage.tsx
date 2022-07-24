d2d8ade587519d3dcf448c12604810f7d0a85073
Butcher Tibi <polecattibi@gmail.com>
2022-07-24 15:35:53 +0300

    Terminat Leafs

--- src/FrontPage/FrontPage.tsx
+++ src/FrontPage/FrontPage.tsx
@@ -1,6 +1,10 @@
-import React, { CSSProperties, useEffect, useState } from "react";
+import React, { CSSProperties, ReactNode, useEffect, useState } from "react";
 
 import './FrontPage.scss';
+import leaf_tex_1 from '../Resources/Leaf_tex_1.jpg';
+import leaf_tex_2 from '../Resources/Leaf_tex_2.jpg';
+import leaf_tex_3 from '../Resources/Leaf_tex_3.jpg';
+import leaf_tex_4 from '../Resources/Leaf_tex_4.jpg';
 
 
 class LeafData {
@@ -9,6 +13,20 @@ class LeafData {
 	z_index: number = 0;
 	size: number = 0;
 	rotation: number = 0;  // radians
+
+	scale_x: number = 1;
+	scale_y: number = 1;
+	side_radius_a: number = 100;
+	side_radius_b: number = 50;
+
+	background: number = 0;
+
+	public static background_images: string[] = [
+		leaf_tex_1,
+		leaf_tex_2,
+		leaf_tex_3,
+		leaf_tex_4
+	];
 }
 
 class ComputedRing {
@@ -19,8 +37,12 @@ class ComputedRing {
 function Leafs()
 {
 	const [inited, setInited] = useState(false);
-	const [begin_rings, setBeginRings] = useState<ComputedRing[]>([]);
-	const [end_rings, setEndRings] = useState<ComputedRing[]>([]);
+
+	// Computed Leafs Data
+	const [open_rings, setOpenRings] = useState<ComputedRing[]>([]);
+	const [close_rings, setCloseRings] = useState<ComputedRing[]>([]);
+
+	const [is_open, setIsOpen] = useState(true);
 
 
 	// Surface Size
@@ -28,37 +50,49 @@ function Leafs()
 	
 	// Ring Params
 	const ring_count = 14;
-	const ring_overlap = 0.5;
-	const ring_begin_size = 100;
-	const ring_angle_offset_growth = 0.5;
+	const ring_open_overlap = -2;
+	const ring_close_overlap = 0.5;
+	const ring_close_size = 100;
+	const ring_open_size = 1100;
+	const ring_closed_angle_offset = 0;
+	const ring_open_angle_offset = Math.PI * 0.5;
+	const ring_angle_offset_growth = Math.PI / 4;
 
 	// Leaf Params
 	const leaf_growth_factor = 1.1;
-	const leaf_overlap = 0.4;
+	const leaf_overlap = 0.5;
+	/** Must remain the same between open and close keyframes or else
+	 * bad performance */
 	const leaf_init_size = 50;
 
+	// Leaf Randomization
+
+
+	const jitter = (min: number, max: number) => {
+		return min + ((max - min) * Math.random());
+	}
 
 	useEffect(() => {
 		if (inited === false) {
 
-			// End Leafs
-			let new_end_rings: ComputedRing[] = [];
+			// Compute closed leafs
+			let new_close_rings: ComputedRing[] = [];
 			{		
-				let angle_offset = 0;
+				let angle_offset = ring_closed_angle_offset;
 				let z_index = 0;
 				let leaf_size = leaf_init_size;
-				let ring_radius = ring_begin_size;
+				let ring_radius = ring_close_size;
 
 				for (let r = 0; r < ring_count; r++) {
 
 					let circum = Math.PI * (ring_radius * 2);
 					let leaf_diagonal = Math.sqrt(Math.pow(leaf_size, 2) * 2);
-					let leaf_count = Math.floor(circum / (leaf_diagonal * (1 - leaf_overlap)));
+					let leaf_count = Math.floor(circum / (leaf_diagonal * leaf_overlap));
 			
 					let angle_rad = angle_offset;
 
-					let new_end_ring = new ComputedRing();
-					new_end_ring.leaf_count = leaf_count;
+					let new_close_ring = new ComputedRing();
+					new_close_ring.leaf_count = leaf_count;
 					
 					for (let i = 0; i < leaf_count; i++) {
 			
@@ -70,50 +104,64 @@ function Leafs()
 						leaf_x += size / 2 - leaf_size / 2;
 						leaf_y += size / 2 - leaf_size / 2;
 				
-						// Size
-				
 						// Rotation
 						let leaf_rotation = (Math.PI / 4) - angle_rad;
+						leaf_rotation += jitter(-0.1, 0.1);
 				
 						// Shape
+						let min_scale  = 1;
+						let max_scale = 1.25;
+						let scale_x = jitter(min_scale, max_scale);
+						let scale_y = jitter(min_scale, max_scale);
 				
+						let side_radius_a = jitter(80, 120);
+						let side_radius_b = jitter(30, 70);
+
+						// Background
+						let background = Math.floor(jitter(0, 4));
+
 						// Store Computation
-						new_end_ring.leafs.push({
+						new_close_ring.leafs.push({
 							leaf_y: leaf_y,
 							leaf_x: leaf_x,
 							z_index: z_index,
 							size: leaf_size,
-							rotation: leaf_rotation
+							rotation: leaf_rotation,
+							scale_x: scale_x,
+							scale_y: scale_y,
+							side_radius_a: side_radius_a,
+							side_radius_b: side_radius_b,
+							background: background
 						});
 				
 						// Advance Leaf
 						angle_rad += (2 * Math.PI) / leaf_count;
 					}
 
-					new_end_rings.push(new_end_ring);
+					new_close_rings.push(new_close_ring);
 			
 					z_index += 1;
 					leaf_size *= leaf_growth_factor;
-					ring_radius += leaf_diagonal * (1 - ring_overlap);
+					ring_radius += leaf_diagonal * (1 - ring_close_overlap);
 					angle_offset += ring_angle_offset_growth;
 				}
 
-				setEndRings(new_end_rings);
+				setCloseRings(new_close_rings);
 			}
 
-			// Begin Leafs
+			// Compute open leafs
 			{
 				let new_begin_rings: ComputedRing[] = [];
 				
-				let angle_offset = 0;
+				let angle_offset = ring_open_angle_offset;
 				let z_index = 0;
 				let leaf_size = leaf_init_size;
-				let ring_radius = 1000;
+				let ring_radius = ring_open_size;
 
 				for (let r = 0; r < ring_count; r++) {
 
 					let leaf_diagonal = Math.sqrt(Math.pow(leaf_size, 2) * 2);
-					let leaf_count = new_end_rings[r].leaf_count;
+					let leaf_count = new_close_rings[r].leaf_count;
 			
 					let angle_rad = angle_offset;
 
@@ -122,6 +170,8 @@ function Leafs()
 					
 					for (let i = 0; i < leaf_count; i++) {
 			
+						let open_leaf = new_close_rings[r].leafs[i];
+
 						// Position
 						let leaf_x = Math.sin(angle_rad) * ring_radius;
 						let leaf_y = Math.cos(angle_rad) * ring_radius;
@@ -142,8 +192,15 @@ function Leafs()
 							leaf_y: leaf_y,
 							leaf_x: leaf_x,
 							z_index: z_index,
+
 							size: leaf_size,
-							rotation: leaf_rotation
+							rotation: leaf_rotation,
+
+							scale_x: open_leaf.scale_x,
+							scale_y: open_leaf.scale_y,
+							side_radius_a: open_leaf.side_radius_a,
+							side_radius_b: open_leaf.side_radius_b,
+							background: open_leaf.background
 						});
 				
 						// Advance Leaf
@@ -154,87 +211,74 @@ function Leafs()
 			
 					z_index += 1;
 					leaf_size *= leaf_growth_factor;
-					ring_radius += leaf_diagonal * (1 - ring_overlap);
+					ring_radius += leaf_diagonal * (1 - ring_open_overlap);
 					angle_offset += ring_angle_offset_growth;
 				}
 
-				setBeginRings(new_begin_rings);
+				setOpenRings(new_begin_rings);
 			}
-			
 			setInited(true);
+		}
+		else {
+			setIsOpen(false);
 		}
 	}, [inited]);
 
 
-	const close = () => {
-		let leafs = document.getElementsByClassName('leaf');
-
-		let leaf_index = 0;
-
-		for (let r = 0; r < ring_count; r++) {
-			
-			let leaf_count = begin_rings[r].leaf_count;
-
-			for (let i = 0; i < leaf_count; i++) {
-
-				let begin = begin_rings[r].leafs[i];
-				let end = end_rings[r].leafs[i];
-
-				leafs[leaf_index].animate([
-					{
-						top: `${begin.leaf_y}px`,
-						left: `${begin.leaf_x}px`,
-					},
-					{
-						top: `${end.leaf_y}px`,
-						left: `${end.leaf_x}px`,
-					}
-				], 2000);
-
-				leaf_index += 1;
-			}
-		}
+	const toggle = () => {
+		setIsOpen(!is_open);
 	}
-
-	// useEffect(() => {
-	// 	let interval_handle: any =  setInterval(animate, 32);
-
-	// 	return () => {
-	// 		clearInterval(interval_handle);
-	// 	}
-	// });
-	
 	
 	// Render
 	if (inited === false) {
 		return null;
 	}
 
-	let leafs: React.ReactNode[] = [];
+	let leafs: ReactNode[] = [];
+	let rings: ComputedRing[] = [];
+
+	if (is_open) {
+		rings = open_rings;
+	}
+	else {
+		rings = close_rings;
+	}
 
 	for (let r = 0; r < ring_count; r++) {
 
-		let leaf_count = end_rings[r].leaf_count;
+		let leaf_count = rings[r].leaf_count;
 
 		for (let i = 0; i < leaf_count; i++) {
 
-			let l = end_rings[r].leafs[i];
+			let l = rings[r].leafs[i];
 	
 			let leaf_style: CSSProperties = {
 				top: `${l.leaf_y}px`,
 				left: `${l.leaf_x}px`,
 				zIndex: `${l.z_index}`,
+
 				border: `1px solid black`,
+				borderTopRightRadius: `${l.side_radius_a}% ${l.side_radius_b}%`,
+				borderBottomLeftRadius: `${l.side_radius_b}% ${l.side_radius_a}%`,
+				
 				width: `${l.size}px`,
 				height: `${l.size}px`,
-				transform: `rotate(${l.rotation}rad)`,
-				backgroundColor: 'green',
+
+				transform: `rotate(${l.rotation}rad) scale(${l.scale_x}, ${l.scale_y})`,
+
+				// backgroundColor: 'green',
 				boxShadow: `0px 0px 5px 1px black`
 			};
+
+			let inner_style: CSSProperties = {
+				backgroundImage: `url('${LeafData.background_images[l.background]}')`,
+			};
+
+			// let key = leafs.length + (is_open ? '_open' : '_close');
 	
 			let new_leaf = (
 				<div key={leafs.length} className="leaf" style={leaf_style}>
-	
+					<div style={inner_style}></div>
 				</div>
 			);
 	
@@ -248,10 +292,8 @@ function Leafs()
 	};
 
 	return (
-		<div className="leafs-wrap" style={{height: `${size}px`}}>
-			<div className="leafs" style={leafs_style}
-				onClick={close}
-			>
+		<div className="leafs-wrap" style={{height: `${size}px`}} onClick={toggle}>
+			<div className="leafs" style={leafs_style}>
 				{leafs}
 			</div>
 		</div>
@@ -261,9 +303,11 @@ function Leafs()
 function FrontPage()
 {
 	return <>
+		<React.StrictMode>
 		<div className="front-page">
 			<Leafs />
 		</div>
+		</React.StrictMode>
 	</>;
 }
 
